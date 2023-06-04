e466f3fb8d415abb92a5a3f45c6d9f38ded6603a
xishengbo <xishengbo@baidu.com>
2023-06-04 16:24:25 +0800

    桂林风云epub生成ok

---
+++ conf.js
@@ -0,0 +1,12 @@
+const zhangdai_list = [
+    'https://mp.weixin.qq.com/mp/homepage?__biz=MzU3Nzg2Njk4MA==&hid=1',
+];
+
+const guilinfenyun_list = [
+    'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzAwMDU0NzkzMg==&action=getalbum&album_id=1652919484384362504&scene=173&from_msgid=2458542270&from_itemidx=1'
+];
+
+module.exports = {
+    zhangdai_list,
+    guilinfenyun_list
+};
